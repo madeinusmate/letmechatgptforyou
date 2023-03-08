f0de26bd9a8827c03568efbf8cfdefbf87a1ca16
madeinusmate <cantu.ste@gmail.com>
2023-03-08 21:18:29 +0800

    added hyperlink and changed footer

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
+import { Text } from "@vercel/examples-ui";
 import { Button } from "./Button";
-import { type ChatGPTMessage, ChatLine, LoadingChatLine } from "./ChatLine";
-import { useCookies } from "react-cookie";
+import { type ChatGPTMessage, LoadingChatLine } from "./ChatLine";
 import { db } from "../firebase";
 import { collection, addDoc } from "firebase/firestore";
 import { Toaster, toast } from "react-hot-toast";
@@ -83,27 +83,38 @@ export function Chat() {
   };
 
   const ShareLink = () => (
-    <div className="rounded-2xl border-zinc-100 flex justify-between items-center mt-4 border py-2">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        className="fill-gray-500 ml-2"
-      >
-        <path d="M8.465 11.293c1.133-1.133 3.109-1.133 4.242 0l.707.707 1.414-1.414-.707-.707c-.943-.944-2.199-1.465-3.535-1.465s-2.592.521-3.535 1.465L4.929 12a5.008 5.008 0 0 0 0 7.071 4.983 4.983 0 0 0 3.535 1.462A4.982 4.982 0 0 0 12 19.071l.707-.707-1.414-1.414-.707.707a3.007 3.007 0 0 1-4.243 0 3.005 3.005 0 0 1 0-4.243l2.122-2.121z"></path>
-        <path d="m12 4.929-.707.707 1.414 1.414.707-.707a3.007 3.007 0 0 1 4.243 0 3.005 3.005 0 0 1 0 4.243l-2.122 2.121c-1.133 1.133-3.109 1.133-4.242 0L10.586 12l-1.414 1.414.707.707c.943.944 2.199 1.465 3.535 1.465s2.592-.521 3.535-1.465L19.071 12a5.008 5.008 0 0 0 0-7.071 5.006 5.006 0 0 0-7.071 0z"></path>
-      </svg>
-      <input
-        className="w-full ml-2 outline-none bg-transparent"
-        type="text"
-        placeholder="link"
-        value={`https://www.letmechatgptforyou.com/share/${shareId}`}
-      />
-      <Button className="mx-4 flex-none" onClick={() => copyLinkToClipboard()}>
-        Copy
-      </Button>
-    </div>
+    <>
+      <div className="rounded-2xl border-zinc-100 flex justify-between items-center mt-4  lg:border py-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          className="fill-gray-500 ml-2"
+        >
+          <path d="M8.465 11.293c1.133-1.133 3.109-1.133 4.242 0l.707.707 1.414-1.414-.707-.707c-.943-.944-2.199-1.465-3.535-1.465s-2.592.521-3.535 1.465L4.929 12a5.008 5.008 0 0 0 0 7.071 4.983 4.983 0 0 0 3.535 1.462A4.982 4.982 0 0 0 12 19.071l.707-.707-1.414-1.414-.707.707a3.007 3.007 0 0 1-4.243 0 3.005 3.005 0 0 1 0-4.243l2.122-2.121z"></path>
+          <path d="m12 4.929-.707.707 1.414 1.414.707-.707a3.007 3.007 0 0 1 4.243 0 3.005 3.005 0 0 1 0 4.243l-2.122 2.121c-1.133 1.133-3.109 1.133-4.242 0L10.586 12l-1.414 1.414.707.707c.943.944 2.199 1.465 3.535 1.465s2.592-.521 3.535-1.465L19.071 12a5.008 5.008 0 0 0 0-7.071 5.006 5.006 0 0 0-7.071 0z"></path>
+        </svg>
+        <a
+          href={`https://www.letmechatgptforyou.com/share/${shareId}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <input
+            className="w-full ml-2 outline-none bg-transparent"
+            type="text"
+            placeholder="link"
+            value={`https://www.letmechatgptforyou.com/share/${shareId}`}
+          />
+        </a>
+        <Button
+          className="mx-4 flex-none"
+          onClick={() => copyLinkToClipboard()}
+        >
+          Copy
+        </Button>
+      </div>
+    </>
   );
 
   return (
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,27 +3,6 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="fixed bottom-0 text-center h-16 sm:h-20 w-full sm:pt-2 pt-4 border-t mt-5 flex sm:flex-row flex-col justify-between items-center px-3 space-y-3 sm:mb-0 mb-3">
-      <div>
-        Powered by{" "}
-        <a
-          href="https://openai.com/"
-          target="_blank"
-          rel="noreferrer"
-          className="font-bold hover:underline transition underline-offset-2"
-        >
-          OpenAI{" "}
-        </a>
-        and{" "}
-        <a
-          href="https://vercel.com/"
-          target="_blank"
-          rel="noreferrer"
-          className="font-bold hover:underline transition underline-offset-2"
-        >
-          Vercel.
-        </a>
-      </div>
-
       <div className="flex space-x-4 pb-4 sm:pb-0">
         Made with ❤️ by
         <a
