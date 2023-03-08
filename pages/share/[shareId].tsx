d5ab7c3c446501bd43796f900700c4835236c838
madeinusmate <cantu.ste@gmail.com>
2023-03-08 20:28:12 +0800

    added banner

--- pages/share/[shareId].tsx
+++ pages/share/[shareId].tsx
@@ -38,6 +38,7 @@ const ChatGPT = () => {
   const [requestTyped, setRequestTyped] = useState(false);
   const [requestDisplayed, setRequestDisplayed] = useState(false);
   const [responseDisplayed, setResponseDisplayed] = useState(false);
+  const [bannerDisplayed, setBannerDisplayed] = useState(false);
 
   useEffect(() => {
     setTimeout(() => {
@@ -47,6 +48,9 @@ const ChatGPT = () => {
     setTimeout(() => {
       setResponseDisplayed(true);
     }, 6000);
+    setTimeout(() => {
+      setBannerDisplayed(true);
+    }, 9000);
   }, [shareId]);
 
   const InputMessage = () => (
@@ -165,7 +169,29 @@ const ChatGPT = () => {
             </div>
           </div>
         )}
+
         <div className="fixed bottom-0 sm:w-1/2  items-center mx-8 mb-8 sm:mx-0 ">
+          {bannerDisplayed && (
+            <div className="text-center py-4 lg:px-4">
+              <div
+                className="p-2 bg-emerald-800 items-center text-white leading-none lg:rounded-full flex lg:inline-flex"
+                role="alert"
+              >
+                <a href="https://chat.openai.com/" target={"_blank"}>
+                  <span className="font-semibold mr-2 text-left flex-auto">
+                    😜 It wasn't that hard? Try ChatGPT now!
+                  </span>
+                </a>
+                <svg
+                  className="fill-current opacity-75 h-4 w-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z" />
+                </svg>
+              </div>
+            </div>
+          )}
           <InputMessage />
         </div>
       </div>
